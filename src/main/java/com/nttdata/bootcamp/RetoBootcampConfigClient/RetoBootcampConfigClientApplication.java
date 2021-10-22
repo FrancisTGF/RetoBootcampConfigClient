d18b57d2d00a8490fab1471e70cae29a5cf9e5f8
Francisco Tomas Guzman Fernandez <francistomas96@gmail.com>
2021-10-22 12:25:32 +0200

    first commit

---
+++ src/main/java/com/nttdata/bootcamp/RetoBootcampConfigClient/RetoBootcampConfigClientApplication.java
@@ -0,0 +1,14 @@
+package com.nttdata.bootcamp.RetoBootcampConfigClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RetoBootcampConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoBootcampConfigClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/bootcamp/RetoBootcampConfigClient/RetoBootcampConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.RetoBootcampConfigClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoBootcampConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
